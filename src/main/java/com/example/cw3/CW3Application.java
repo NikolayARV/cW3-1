0aa035afa2db4b1b53c822f9878c378c87063f5d
razenkov <razenkov.rna@mail.ru>
2023-02-14 20:53:43 +0300

    Initial commit

---
+++ src/main/java/com/example/cw3/CW3Application.java
@@ -0,0 +1,13 @@
+package com.example.cw3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CW3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CW3Application.class, args);
+    }
+
+}
